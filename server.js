3bc9f633d93b2ae1b168a4fad108b5caa3792a22
Ryan Flach <ryanflach@users.noreply.github.com>
2016-10-19 10:14:30 -0600

    Add export and conditional to server - Only listen if the server is accessed directly

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
+// Only listen if the server is being accessed directly (i.e., not via the test suite)
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
